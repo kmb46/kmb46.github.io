b4cf873889b792e87a4625594bbc9fc650d0a540
Mohamed El Bachir KHOUSSA <bachir4694@gmail.com>
2020-03-14 20:58:41 +0100

    [feat] root: add the possibility to switch between light and dark modes

---
+++ js/theme_switcher_script.js
@@ -0,0 +1,26 @@
+function setActiveStyleSheet(title) {
+    var i, a, main;
+    for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+      if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title")) {
+        a.disabled = true;
+        if(a.getAttribute("title") == title) a.disabled = false;
+      }
+    }
+}
+
+function getActiveStyleSheet() {
+    var i, a;
+    for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
+      if(a.getAttribute("rel").indexOf("style") != -1 && a.getAttribute("title") && !a.disabled) return a.getAttribute("title");
+    }
+    return null;
+}
+
+function switchTheme() {
+    var slider = document.getElementById("toggle-switch");
+    if (slider.checked) {
+      setActiveStyleSheet('dark_mode');
+    } else {
+      setActiveStyleSheet('light_mode');
+    }
+}
